03f6082f58530f61a69729b6556cefcac51e2d07
Mikel <mwaldie01@gmail.com>
2021-06-07 16:29:31 +1000

    0006: Feature-Game(bonus point collectables): Point giving collectables have been introduced, it works.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField]
+    private float points;
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<GameManager>().score += points;
+        Destroy(gameObject);
+    }
+}
